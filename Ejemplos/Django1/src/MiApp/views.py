699478375af58029db7dd88c594ecc2210f4a987
Juan Luis Campos Barrera <luiscampos@uaz.edu.mx>
2026-02-19 09:11:10 -0600

    Sesion templates

--- Ejemplos/Django1/src/MiApp/views.py
+++ Ejemplos/Django1/src/MiApp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from MiApp.models import PersonaModel, PersonaDatosGenerales
-
+from django.contrib.auth.decorators import login_required
 def HolaMundo(request):
-    return HttpResponse("Hola Mundo cruel y despiadado")
+    contexto = {
+        'title': ':: Bienvenidos todos'
+    }
+    return render(request,'main/base.html',contexto)
 
 def get_fecha_hora(request):
     return HttpResponse(datetime.now())
@@ -11,11 +14,16 @@ def get_fecha_hora(request):
 def ListaPersonas(request):
     listaPersonas = PersonaModel.objects.all()
     contexto = {
-        'title': ':: Lista de Personas',
+        'title': ':: Lista de Personas - hola',
         'listaPersonas': listaPersonas
     }
-    return render(request,'main/base.html',contexto)
+    return render(request,'miapp/ListaPersonas.html',contexto)
 
+@login_required(login_url='/admin')
 def DatosGenerales(request,persona_id):
-    datosPersona = PersonaDatosGenerales.objects.get(persona_id=persona_id)
-    return HttpResponse(datosPersona)
+    datosPerDetallePersonasona = PersonaDatosGenerales.objects.get(persona_id=persona_id)
+    contexto = {
+        'title': ':: Datos Generales Por Persona',
+        'datosPersona': datosPerDetallePersonasona
+    }
+    return render(request, 'miapp/DatosGeneralesPersona.html',contexto)
